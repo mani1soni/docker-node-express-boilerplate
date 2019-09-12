f0ee8e7d65b9c1f059ccb8ff15bd5f1be12ab387
mani1soni <manish.soni8403@gmail.com>
2019-09-12 12:34:14 +0530

    Jenkinsfile updated

--- stage-build-and-deploy.sh
+++ stage-build-and-deploy.sh
@@ -3,7 +3,7 @@
 while [ "BUILD"="true" ]
 do
 
-    if [ ! "$(docker ps -a | grep ${NODE_ENV}-$1)" ]; then
+    if [ ! "$(docker ps -a | grep ${NODE_ENV}$1)" ]; then
 	echo "container does not exist"
 	CONTAINER=false
     else
@@ -11,7 +11,7 @@ do
 	CONTAINER=true
     fi
 
-    if [ ! "$(docker images | grep ${NODE_ENV}-$1)" ]; then
+    if [ ! "$(docker images | grep ${NODE_ENV}$1)" ]; then
         echo "image does not exist"
 	    IMAGE=false
     else
@@ -20,12 +20,12 @@ do
     fi
 
     if [[ $CONTAINER = "true" ]] && [[ $IMAGE = "true" ]]; then
-        docker rm -f ${NODE_ENV}-$1
-        docker rmi ${NODE_ENV}-$1
+        docker rm -f ${NODE_ENV}$1
+        docker rmi ${NODE_ENV}$1
         BUILD=start
 
     elif [[ $CONTAINER = "false" ]] && [[ $IMAGE = "true" ]]; then
-        docker rmi ${NODE_ENV}-$1
+        docker rmi ${NODE_ENV}$1
         BUILD=start
 
     elif [[ $CONTAINER = "true" ]] && [[ $IMAGE = "false" ]]; then
@@ -37,7 +37,7 @@ do
 
     if [ $BUILD = "start" ]; then 
 	echo "container and image removed run build steps"
-    docker build -t ${NODE_ENV}-$1 .
+    docker build -t ${NODE_ENV}$1 .
     docker run -d -p $2:$2 -v /home/ubuntu/logs:/root/.pm2/logs --name ${NODE_ENV}-$1 ${NODE_ENV}-$1
     else
 	echo "container or image does not removed build steps are not going to run"
