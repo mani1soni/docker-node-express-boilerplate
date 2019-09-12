c4511dccfcae03e80ec49e59308d9ab1f79fefdb
mani1soni <manish.soni8403@gmail.com>
2019-09-12 19:14:34 +0530

    Jenkinsfile updated

--- push.sh
+++ push.sh
@@ -5,8 +5,8 @@ docker tag $1:latest $2:latest
 docker tag $1:latest $2:$BUILD_NUMBER
 
 #pushing it on registry
-docker push $2:latest && /
-docker push $2:$BUILD_NUMBER && /
+docker push $2:latest && \
+docker push $2:$BUILD_NUMBER && \
 
 #removing from local server
 docker rmi $2:latest $2:$BUILD_NUMBER
