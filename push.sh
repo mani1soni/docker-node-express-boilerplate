6970c8fa54d644941b5fec1b6672010febf57a84
mani1soni <manish.soni8403@gmail.com>
2019-09-12 14:58:51 +0530

    script updated

--- push.sh
+++ push.sh
@@ -2,11 +2,12 @@
 
 #tagging the image with registry name
 docker tag $1:latest $2:latest
-docker tag $1:latest $2:${BUILD_NUMBER}
+#docker tag $1:latest $2:${BUILD_NUMBER}
 
 #pushing it on registry
-docker push $2:latest && /
-docker push $2:${BUILD_NUMBER} && /
+docker push $2:latest 
+#&& /
+#docker push $2:${BUILD_NUMBER} && /
 
 #removing from local server
-docker rmi $2:latest $2:${BUILD_NUMBER}
+docker rmi $2:latest 
