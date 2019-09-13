93c17382153d2097d13037b544e0f442f9f6b0af
mani1soni <manish.soni8403@gmail.com>
2019-09-13 10:31:29 +0530

    script edited

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #tagging the image with registry name
-docker tag $1:latest ${NODE_ENV}-$2:latest
-docker tag $1:latest ${NODE_ENV}-$2:$BUILD_NUMBER
+docker tag $1:latest $2-${NODE_ENV}:latest
+docker tag $1:latest $2-${NODE_ENV}:$BUILD_NUMBER
 
 #pushing it on registry
-docker push ${NODE_ENV}-$2:latest && \
-docker push ${NODE_ENV}-$2:$BUILD_NUMBER && \
+docker push $2-${NODE_ENV}:latest && \
+docker push $2-${NODE_ENV}:$BUILD_NUMBER && \
 
 #removing from local server
-docker rmi ${NODE_ENV}-$2:latest ${NODE_ENV}-$2:$BUILD_NUMBER
+docker rmi $2-${NODE_ENV}:latest $2-${NODE_ENV}:$BUILD_NUMBER
