b7903d596f32fc96896a813b069aa5161605f114
mani1soni <manish.soni8403@gmail.com>
2019-09-13 16:34:19 +0530

    prod files added

--- prod-backup.sh
+++ prod-backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #tagging current image to backup image 
-docker tag $1:latest $2:latest
+docker tag $1:latest $2:latest 
 docker tag $1:latest $2:$BUILD_NUMBER
 
 #pushing images to registry
-docker push $2:latest && /
-docker push $2:$BUILD_NUMBER && /
+docker push $2:latest && \
+docker push $2:$BUILD_NUMBER && \
 #if push successfull then 
 docker rmi $2:latest $2:$BUILD_NUMBER
 echo "backup done"
