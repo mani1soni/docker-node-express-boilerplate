c40e7f70c49faa3e1d54ce94b312d6d6ffd97422
mani1soni <manish.soni8403@gmail.com>
2019-09-14 15:09:12 +0530

    script updated

--- stage-rollback.sh
+++ stage-rollback.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 if [ ! "$(docker ps -a | grep $1)" ]; then
-echo "container does not exist"
 cd /home/manish/savetest/Staging/backup
 docker load < $1.tar
 docker run -d -p $2:$2 -v /home/ubuntu/logs:/root/.pm2/logs --name $1 $1
--- testecr.sh
+++ testecr.sh
@@ -36,7 +36,7 @@ if [ $BUILD = "start" ]; then
 echo "container and image removed run build steps"
 docker tag $1-jenkins $1
 docker run -d -p $2:$2 -v /home/ubuntu/logs:/root/.pm2/logs --name $1 $1
-docker rm -f $1-jenkins
+docker rm -f $1-jenkins:$BUILD_ID
 docker rmi $1-jenkins
 else
 echo "container or image does not removed build steps are not going to run"
